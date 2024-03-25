ab570c384cc2de2fc83f0c986a1dc8f31d246f8a
luishrtd <luis.hurtadorab@gmail.com>
2024-03-25 14:13:48 -0600

    Adición de la funcionalidad para desencriptar un texto

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function encriptar(){
+function encriptar() {
 
     let texto = document.getElementById("texto").value;
     let tituloMensaje = document.getElementById("titulo-mensaje");
@@ -12,15 +12,41 @@ function encriptar(){
         .replace(/o/gi, "ober")
         .replace(/u/gi, "ufat");
 
-    if(texto.length != 0){
+    if (texto.length != 0) {
         document.getElementById("texto").value = textoCifrado;
         tituloMensaje.textContent = "Texto encriptado con éxito";
         parrafo.textContent = "";
         muneco.src = "./Imagenes/encriptado.jpg";
-    } else{
-        muneco.src= "./Imagenes/Muñeco.svg";
+    } else {
+        muneco.src = "./Imagenes/Muñeco.svg";
         tituloMensaje.textContent = "Ningún mensaje fue encontrado";
         parrafo.textContent = "Ingresa el texto que deseas encriptar o desencriptar";
         alert("Debes ingresar algún texto");
     }
-}
+}    
+
+function desencriptar() {
+        let texto = document.getElementById("texto").value;
+        let tituloMensaje = document.getElementById("titulo-mensaje");
+        let parrafo = document.getElementById("parrafo");
+        let muneco = document.getElementById('muneco');
+
+        let textoCifrado = texto
+            .replace(/enter/gi, "e")
+            .replace(/imes/gi, "i")
+            .replace(/ai/gi, "a")
+            .replace(/ober/gi, "o")
+            .replace(/ufat/gi, "u");
+
+        if (texto.length != 0) {
+            document.getElementById("texto").value = textoCifrado;
+            tituloMensaje.textContent = "Texto desencriptado con éxito";
+            parrafo.textContent = "";
+            muneco.src = "./Imagenes/desencriptado.jpg";
+        } else {
+            muneco.src = "./Imagenes/Muñeco.svg";
+            tituloMensaje.textContent = "Ningún mensaje fue encontrado";
+            parrafo.textContent = "Ingresa el texto que deseas encriptar o desencriptar";
+            alert("Debes ingresar algún texto");
+        }
+    }
